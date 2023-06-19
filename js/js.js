d35760dca74de740fe000ab55184120ad460cdf0
DorantVelija <dorantvelija@outlook.com>
2023-06-19 17:15:05 +0200

    Save incase

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let element = document.getElementById('aboutText2');
+let element2 = document.getElementById('aboutText2');
+
+function buttonClick(){
+    element.classList.toggle('hide');
+    element2.classList.toggle('aboutText2');
+}
