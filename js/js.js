2fc1b0a6902920db5a756ba63bbf57de10c6db66
DorantVelija <dorantvelija@outlook.com>
2023-06-19 18:15:08 +0200

    added more stuff

--- js/js.js
+++ js/js.js
@@ -5,3 +5,14 @@ function buttonClick(){
     element.classList.toggle('hide');
     element2.classList.toggle('aboutText2');
 }
+
+let buttonshow = document.getElementById('showMore');
+let home = document.getElementById('home')
+let picNo = 0
+function changeBackground() {
+    picNo = picNo ++ ;
+    if(picNo>6){
+    picNo = 0 
+}    
+    home.style.backgroundImage = "url(../IMGg/homepic"+picNo+".png)";
+}
